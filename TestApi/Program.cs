ff8cab7e91966077c2fd4a59f3014538fc9434c1
eiximenis <etomas@gmail.com>
2021-02-19 18:26:13 +0100

    Test api for reference

---
+++ TestApi/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Logfmt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog((hostBuilderContext, config) =>
+                {
+                    config.MinimumLevel.Verbose()
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console(new LogfmtFormatter());
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
