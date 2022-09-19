c74fd35bb56750e34325b8f2831076290738d494
Olve S. Hansen <olvesh@gmail.com>
2022-09-19 13:11:24 +0200

    Added quotes handling on values and msg

--- Serilog.Logfmt/LogfmtValueFormatter.cs
+++ Serilog.Logfmt/LogfmtValueFormatter.cs
@@ -45,7 +45,7 @@ namespace Serilog.Logfmt
             var keyName = GetFullKeyName();
             var svalue = scalar.Value?.ToString() ?? "\"\"";
             state.Write("{0}=", keyName);
-            var needQuotes = svalue.Contains(" ");
+            var needQuotes = svalue.Contains(" ") || svalue.Contains("=");
             if (needQuotes)
             {
                 switch (_options.DoubleQuotesAction)
